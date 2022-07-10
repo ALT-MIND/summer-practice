6a8ee6a91cd968f92b0008815bade47db862bf2b
ALT-MIND <altmind.inb@gmail.com>
2022-07-11 00:09:51 +0300

    Невеликі зміни в алгоритмі BFS

--- pathfind/bfs.c
+++ pathfind/bfs.c
@@ -20,12 +20,15 @@ int find_patch(int board[], pos start, pos stop, int row, int col){
         if (current.x == stop.x && current.y == stop.y) {
             cell t = board_clone[stop.y][stop.x];
             for (;;) {
-                if (t.prev_step_cell_pos.y == -1 && t.prev_step_cell_pos.x == -1) return 1;
+                if (board_clone[t.prev_step_cell_pos.y][t.prev_step_cell_pos.x].prev_step_cell_pos.y == -1 &&
+                                board_clone[t.prev_step_cell_pos.y][t.prev_step_cell_pos.x].prev_step_cell_pos.x == -1) return 1;
                 board[col * t.prev_step_cell_pos.y + t.prev_step_cell_pos.x] = 2;
                 t = board_clone[t.prev_step_cell_pos.y][t.prev_step_cell_pos.x];
             }
         }
         for (q = 0; q < 4; q++){
+            if (current.y + delta_y[q] < 0 || current.y + delta_y[q] > row - 1) continue;
+            if (current.x + delta_x[q] < 0 || current.x + delta_x[q] > col - 1) continue;
             if (board_clone[current.y + delta_y[q]][current.x + delta_x[q]].visited == 'w') {
                 continue;
                 }
